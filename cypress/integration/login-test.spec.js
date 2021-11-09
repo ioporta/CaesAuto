21afe4d315e5bed659f4719d8d830d39d3aec531
Ivannia Oporta Quesada <ioporta@cajadeande.fi.cr>
2021-11-09 17:12:35 -0600

    framework base IOQ

---
+++ cypress/integration/login-test.spec.js
@@ -0,0 +1,87 @@
+/// <reference types="cypress" />
+
+before('Cargar Data', () => {
+    cy.fixture('login').then(function (login) {
+        this.login = login;
+    })
+})
+
+describe('hacer registro en e-commerce', () => {
+    
+    it('exitoso', function () {
+        cy.visit('http://automationpractice.com/index.php');
+        cy.get('.login').click()
+        cy.get('#email_create').type('juanduarte1612@gmail.com')
+        cy.get('#SubmitCreate > span').click()
+        cy.wait(2000)
+        cy.get('#id_gender1').click()
+        cy.get('#customer_firstname').type('Pepito')
+        cy.get('#customer_lastname').type('Perez')
+        cy.get('#passwd').type('Pepito123')
+        cy.get('#address1').type('cualquier direccion')
+        cy.get('#city').type('Bogota')
+        cy.get('#phone_mobile').type('222546400')
+        cy.get('#id_state').select('Alabama')
+        cy.get('#postcode').type('00000')
+        cy.get('#submitAccount > span').click()
+        cy.get('.myaccount-link-list > :nth-child(4) > a > span').should('be.visible')        
+    })
+
+    it('exitoso con variables', function () {
+        cy.visit('http://automationpractice.com/index.php');
+        cy.get('.login').click()
+        cy.get('#email_create').type(this.login.correoRegistroExitoso)
+        cy.get('#SubmitCreate > span').click()
+        cy.wait(2000)
+        cy.get('#id_gender1').click()
+        cy.get('#customer_firstname').type(this.login.nombre)
+        cy.get('#customer_lastname').type(this.login.apellido)
+        cy.get('#passwd').type(this.login.clave)
+        cy.get('#address1').type(this.login.direccion)
+        cy.get('#city').type(this.login.ciudad)
+        cy.get('#phone_mobile').type(this.login.telefono)
+        cy.get('#id_state').select(this.login.estado)
+        cy.get('#postcode').type(this.login.zip)
+        cy.get('#submitAccount > span').click()
+        cy.get('.myaccount-link-list > :nth-child(4) > a > span').should('be.visible')        
+    })
+
+    it('fallido por código postal', function () {
+        cy.visit('http://automationpractice.com/index.php');
+        cy.get('.login').click()
+        cy.get('#email_create').type('juanduarte3012@gmail.com')
+        cy.get('#SubmitCreate > span').click()
+        cy.get('#id_gender1').click()
+        cy.get('#customer_firstname').type('Pepito')
+        cy.get('#customer_lastname').type('Perez')
+        cy.get('#passwd').type('Pepito123')
+        cy.get('#address1').type('cualquier direccion')
+        cy.get('#city').type('Bogota')
+        cy.get('#phone_mobile').type('222546400')
+        cy.get('#id_state').select('Alabama')
+        cy.get('#postcode').type('000000')
+        cy.get('#submitAccount > span').click()
+        cy.get('ol > li').should('contain', 'It must follow this format: 00000')     
+    })
+
+    it('hacer login', function () {
+        cy.visit('http://automationpractice.com/index.php');
+        cy.get('.login').click()
+        cy.get('#email').type('juanduarte1612@gmail.com')
+        cy.get('#passwd').type('Pepito123')
+        cy.get('#SubmitLogin > span').click()
+        //cy.get('.myaccount-link-list > :nth-child(4) > a > span').should('be.visible')        
+    })
+
+
+    it('hacer login con contraseña incorrecto', function () {
+        cy.visit('http://automationpractice.com/index.php');
+        cy.get('.login').click()
+        cy.get('#email').type('juanduarte1612@gmail.com')
+        cy.get('#passwd').type('Prueba123')
+        cy.get('#SubmitLogin > span').click()
+        cy.get('ol > li').should('contain', 'Authentication failed.')     
+    })
+
+
+})
